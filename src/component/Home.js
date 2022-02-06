ece4cf9009fe726ecf128d9ce00e286c6655eb39
akshad113 <akshad113@gmail.com>
2022-02-06 21:38:17 +0530

    product are added

--- src/component/Home.js
+++ src/component/Home.js
@@ -6,21 +6,34 @@ export default function Home() {
     <div className="home">
       <div className="home__container">
         <img
-          className="home__img"
-          src="https://m.media-amazon.com/images/I/61FuWeCuGCL._SX3000_.jpg "
+          className="home__image"
+          src="https://m.media-amazon.com/images/I/61FuWeCuGCL._SX3000_.jpg"
+          alt=""
         />
         <div className="home__row">
-          <Product />
-          {/* product */}
+          <Product
+            title="Northen Farm"
+            image="https://m.media-amazon.com/images/I/5139RPK39ZL._AC_UY218_.jpg"
+            price={299}
+            rating={4}
+          />
+          <Product
+            title="OnePlus Nord 2 5G"
+            image="https://m.media-amazon.com/images/I/61TnX0PmqES._AC_UL320_.jpg"
+            price={299}
+            rating={4}
+          />
         </div>
 
         <div className="home__row">
-          {/* product */}
-          {/* product */}
-          {/* product */}
+          <Product />
+          <Product />
+          <Product />
         </div>
 
-        <div className="home__row">{/* product */}</div>
+        <div className="home__row">
+          <Product />
+        </div>
       </div>
     </div>
   );
--- src/component/Product.js
+++ src/component/Product.js
@@ -1,15 +1,26 @@
 import React from "react";
 import "../css/Product.css";
 
-export default function Product() {
+export default function Product({ title, image, price, rating }) {
   return (
     <div className="product">
       <div className="product__info">
+        <p>{title}</p>
         <p className="product__price">
-          <small>$</small>
-          <strong>19.99</strong>
+          <small>₹</small>
+          <strong>{price}</strong>
         </p>
+        <div className="product__rating">
+          {Array(rating)
+            .fill()
+            .map((_, i) => (
+              <p>⭐</p>
+            ))}
+        </div>
       </div>
+
+      <img src={image} />
+      <button>Add to Basket</button>
     </div>
   );
 }
